f08e4f96c9f41370a97e11a019c6e8d3b2688d09
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-23 23:12:08 +0300

    feat: create `DetailViewController`

---
+++ corpses-of-mexico/corpses-of-mexico/ViewControllers/DetailViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
