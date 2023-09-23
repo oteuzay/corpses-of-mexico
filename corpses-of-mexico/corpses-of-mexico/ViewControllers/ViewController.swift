ddfbd91b592e803ce684df3aaee01b6abf70a33c
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-23 22:09:30 +0300

    feat: create `loadPictures()` function and set it up

--- corpses-of-mexico/corpses-of-mexico/ViewControllers/ViewController.swift
+++ corpses-of-mexico/corpses-of-mexico/ViewControllers/ViewController.swift
@@ -1,13 +1,29 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         configureNavigation()
+        loadPictures()
     }
     
     func configureNavigation() -> Void {
         self.title = "Corpses of Mexico"
+    }
+    
+    func loadPictures() -> Void {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let pictures = try! fm.contentsOfDirectory(atPath: path)
+        
+        for picture in pictures {
+            if picture.hasSuffix(".jpeg") || picture.hasSuffix(".jpg") {
+                self.pictures.append(picture)
+            }
+        }
     }
 }
