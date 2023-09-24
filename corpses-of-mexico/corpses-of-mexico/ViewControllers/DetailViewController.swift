b368cfcacbaab569e1921dd2e75179ace895d3c9
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-24 23:42:14 +0300

    feat: add `viewWillAppear`and `viewWillDisappear` functions

--- corpses-of-mexico/corpses-of-mexico/ViewControllers/DetailViewController.swift
+++ corpses-of-mexico/corpses-of-mexico/ViewControllers/DetailViewController.swift
@@ -17,6 +17,16 @@ class DetailViewController: UIViewController {
         self.title = "Picture"
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
     private func imageToLoad(selectedImage: String?) -> Void {
         if let imageToLoad = selectedImage {
             imageView.image  = UIImage(named: imageToLoad)
