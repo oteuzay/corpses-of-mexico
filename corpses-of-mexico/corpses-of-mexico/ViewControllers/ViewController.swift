75d3698d2cd91eaf3eaebda7886dca67b60b7ed6
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-24 23:15:03 +0300

    feat: update pictures order and configure `didSelectRowAt`

--- corpses-of-mexico/corpses-of-mexico/ViewControllers/ViewController.swift
+++ corpses-of-mexico/corpses-of-mexico/ViewControllers/ViewController.swift
@@ -25,6 +25,8 @@ class ViewController: UITableViewController {
                 self.pictures.append(picture)
             }
         }
+        
+        self.pictures.sort()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -37,5 +39,13 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = pictures[indexPath.row]
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            
+            vc.selectedImage = self.pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
