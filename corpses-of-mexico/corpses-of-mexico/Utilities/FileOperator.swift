51bd87810a08f05c0e1bef5d85a9ade5518a6042
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-25 19:15:05 +0300

    refactor: :recycle: rename `fileTypes` to `fileFormats` in `getFiles()`

--- corpses-of-mexico/corpses-of-mexico/Utilities/FileOperator.swift
+++ corpses-of-mexico/corpses-of-mexico/Utilities/FileOperator.swift
@@ -4,15 +4,15 @@ class FileOperator {
     
     private var files = [String]()
     
-    func getFiles(fileTypes: [String]) -> [String] {
+    func getFiles(fileFormats: [String]) -> [String] {
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let files = try! fm.contentsOfDirectory(atPath: path)
         
         for file in files {
-            for fileType in fileTypes {
-                if file.hasSuffix(fileType) {
+            for fileFormat in fileFormats {
+                if file.hasSuffix(fileFormat) {
                     self.files.append(file)
                 }
             }
